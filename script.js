2c78099f30938c3514161d54ba2dabaf9181cc81
Стасік Петрмк <stasps210@gmail.com>
2024-03-29 22:04:42 +0200

    update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var canvas = document.getElementById('game-life')
+var canvas = document.getElementById('maze-generator')
 var ctx = canvas.getContext('2d')
 
 canvas.width = window.innerWidth;
@@ -9,45 +9,71 @@ let path = []
 let wall = []
 let cells = []
 const size = 25
-const mazeSize = 25
+const mazeSize = 4
 
 function randomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-for (y = 0; y < mazeSize; y++) {
-  for (x = 0; x < mazeSize; x++) {
-    cells.push({
-      wall: 1,
-      path: 0,
-      special: 0,
-      x: x,
-      y: y,
-      index: x + y * mazeSize
-    })
-  }
-}
-
-function generate(arr) {
-  arr[0].wall = 0
-  arr[0].path = 1
-  for (i = 0; i < arr.length; i++) {
-    if (arr[i] ) {
-
+function linearSearch(array, item) {
+  for (i = 0; i < mazeSize; i++) {
+    for (j = 0; j < mazeSize; j++) {
+      if (array[i][j].special === item) {
+        return true
+      }
     }
   }
 }
 
+
+//i=y j=x
+for (i = 0; i < mazeSize; i++) {
+  cells[i] = []
+  for (j = 0; j < mazeSize; j++) {
+    cells[i][j] = {
+      wall: 1,
+      path: 0,
+      special: 0,
+      y: i,
+      x: j
+    }
+  }
+}
+
+cells[0][0].wall = 0
+cells[0][0].path = 1
+
+function generate(array) {
+  if (linearSearch(cells, 1) === true) {
+    return array
+  }
+
+  randomInt()
+  for (i = 0; i < mazeSize; i++) {
+    for (let j = 0; j < mazeSize; j++) {
+      if (cells[i][j].path == 1) {
+        cells[i][j + 2].wall = 0
+        cells[i + 2][j].wall = 0
+        cells[i][j + 2].special = 1
+        cells[i + 2][j].special = 1
+      }
+    }
+  }
+  return generate(array)
+}
+
 generate(cells)
 
-function paint(arr) {
-  for (i = 0; i < cells.length; i++) {
-    if (cells[i].wall == 1) {
-      ctx.fillStyle = 'black'
-      ctx.fillRect(cells[i].x * size, cells[i].y * size, size, size)
-    } else if(cells[i].path == 1) {
-      ctx.fillStyle = 'white'
-      ctx.fillRect(cells[i].x * size, cells[i].y * size, size, size)
+function paint(array) {
+  for (i = 0; i < mazeSize; i++) {
+    for (j = 0; j < mazeSize; j++) {
+      if (array[i][j].wall == 1) {
+        ctx.fillStyle = 'black'
+        ctx.fillRect(array[i][j].x * size, array[i][j].y * size, size, size)
+      } else {
+        ctx.fillStyle = 'white'
+        ctx.fillRect(array[i][j].x * size, array[i][j].y * size, size, size)
+      }
     }
   }
 }
