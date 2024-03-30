8ea45e94207c47ecee88485a3aeb3972bc623c6d
Стасік Петрмк <stasps210@gmail.com>
2024-03-30 18:25:53 +0200

    update

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ canvas.height = window.innerHeight;
 let special = []
 let cells = []
 const size = 25
-const mazeSize = 15
+const mazeSize = 5
 
 function randomInt(max) {
   return Math.floor(Math.random() * max);
@@ -20,9 +20,8 @@ for (i = 0; i < mazeSize; i++) {
     cells[i][j] = {
       wall: 1,
       path: 0,
-      // special: 0,
+      x: j,
       y: i,
-      x: j
     }
   }
 }
@@ -31,20 +30,24 @@ cells[0][0].wall = 0
 cells[0][0].path = 1
 
 function generate(array) {
+  //generate all special
+
   for (i = 0; i < mazeSize; i++) {
-    for (let j = 0; j < mazeSize; j++) {
+    for (j = 0; j < mazeSize; j++) {
       if (array[i][j].path == 1) {
-        if (array[i][j + 2] != null && array[i][j + 2].path != 1) {
+        if (array[i][j + 2] != undefined && array[i][j + 2].wall == 1) {
           array[i][j + 2].wall = 0
           special.push(array[i][j + 2])
         }
-        if (array[i + 2][j] != null && array[i + 2][j].path != 1) {
+        if (array[i + 2][j] != undefined && array[i + 2][j].wall == 1) {
           array[i + 2][j].wall = 0
           special.push(array[i + 2][j])
         }
       }
     }
   }
+
+  //random special to path
 
   let randomSpecial = randomInt(special.length)
   
@@ -53,20 +56,22 @@ function generate(array) {
       if (array[i][j].x == special[randomSpecial].x && array[i][j].y == special[randomSpecial].y) {
         array[i][j] = special[randomSpecial]
         array[i][j].path = 1
-        // special[randomSpecial] = null
       }
     }
   }
 
-  
+  //recursive
+
   if (special.length <= 0) {
     return array
   } else {
     special.splice(0, special.length)
   }
-  return generate(array)
+  // return generate(array)
 }
 
+generate(cells)
+generate(cells)
 generate(cells)
 
 
